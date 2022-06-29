92579b5790b90659b99aabee056593b29bb7824d
Yu Liang <liangyusam@gmail.com>
2022-06-29 19:57:07 +1000

    .

--- backend/db.py
+++ backend/db.py
@@ -4,6 +4,7 @@ from pyparsing import dbl_quoted_string
 def db_connect():
     client = pymongo.MongoClient("mongodb+srv://sam:sam@cluster0.ubskf.mongodb.net/?retryWrites=true&w=majority")
     db_set = client.test
+    db = db_set['mongo_db']
     return db
 
 
