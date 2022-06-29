78538f3b365694259501b02b037ecd6febe7f9ff
Yu Liang <liangyusam@gmail.com>
2022-06-29 19:53:52 +1000

    init

--- backend/db.py
+++ backend/db.py
@@ -1,13 +1,10 @@
 import pymongo
+from pyparsing import dbl_quoted_string
 
 def db_connect():
-    # client = pymongo.MongoClient("mongodb+srv://sam:sam@cluster0.sjc0p.mongodb.net/?retryWrites=true&w=majority")
-    # db = client.sample_airbnb
-    import pymongo
-
-
     client = pymongo.MongoClient("mongodb+srv://sam:sam@cluster0.ubskf.mongodb.net/?retryWrites=true&w=majority")
-    db = client.test
+    db_set = client.test
+    db = db_set['mongo_db']
     return db
 
 
