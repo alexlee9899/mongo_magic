20c78adc7edf0263d6baeef7d33d53d0696d745d
jinl96 <jinyangliu1996@gmail.com>
2022-07-07 02:32:45 +0800

    shutting down some eslint warnings

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useSearchParams } from 'react-router-dom';
 import { message } from 'antd';
 import Dashboard from "./Dashboard";
