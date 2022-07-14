44c1139cf2614cf6cf3fe17fdffe06e47406491f
jinl96 <jinyangliu1996@gmail.com>
2022-07-14 14:29:20 +0800

    profile update implemented.

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -6,6 +6,7 @@ import HeaderBar from '../component/HeaderBar'
 import uploadAvatar from '../assets/uploadAvatar.png';
 import { ProfileContext } from '../App';
 import { fileToDataUrl } from "../utils/functions";
+import { updateProfile } from "../utils/requests";
 
 const { Content } = Layout;
 const { Dragger } = Upload;
@@ -69,6 +70,7 @@ export default function Profile() {
                     .then(response => {
                         setImgUrl(response);
                         setImgReady(false);
+                        setUser({ ...user, photo: response });
                         info.file.status = 'done';
                     });
             }
@@ -81,6 +83,17 @@ export default function Profile() {
         showUploadList: false,
     }
 
+    const update = (data) =>{
+        updateProfile(data)
+            .then(res => {
+                if (res.ok) {
+                    message.success('Profile updated successfully');
+                }
+                else {
+                    message.error('Error updating profile');
+                }
+            })
+    }
     return (
         <>
             {(prof.providerProfile.profile) ? (
@@ -91,23 +104,23 @@ export default function Profile() {
                     <div>
                     <Dragger {...props}>
                         <div style={{backgroundColor:'white', borderRadius:'100%', width:'300px', height:'300px'}}>
-                            <img style={{borderRadius:'100%', width:'300px', height:'300px'}} src={imgUrl ? imgUrl : uploadAvatar}></img>
+                            <img alt='avatar' style={{borderRadius:'100%', width:'300px', height:'300px'}} src={imgUrl ? imgUrl : uploadAvatar}></img>
                         </div>
                     </Dragger>
                     </div>
                     <div style={{width:'40vw', marginTop:'100px'}}>
                     <h3> Name</h3>
-                    <Input size="large" maxLength='20' style={{width:'40vw'}} value={prof.providerProfile.profile.fullname}/>
+                    <Input size="large" maxLength='20' style={{width:'40vw'}} defaultValue={prof.providerProfile.profile.fullname} onChange={(e) => setUser({ ...user, fullname: e.target.value })} />
                     </div>
                     <div style={{width:'40vw', marginTop:'20px'}}>
                     <h3> Email(required)</h3>
-                    <Input size="large" maxLength='40' style={{width:'40vw'}} value={prof.providerProfile.profile.email}/>
+                    <Input size="large" maxLength='40' style={{width:'40vw'}} defaultValue={prof.providerProfile.profile.email} onChange={(e) => setUser({ ...user, email: e.target.value })}/>
                     </div>
                     <div style={{width:'40vw', marginTop:'20px'}}>
                     <h3> Organisation Name</h3>
-                    <Input size="large" maxLength='50' style={{width:'40vw'}} value={prof.providerProfile.profile.org}/>
+                    <Input size="large" maxLength='50' style={{width:'40vw'}} defaultValue={prof.providerProfile.profile.org} onChange={(e) => setUser({ ...user, org: e.target.value })}/>
                     </div>
-                    <Button style={uploadStyle}>Update My Profile</Button>
+                    <Button style={uploadStyle} onClick={() => (update(user))}>Update My Profile</Button>
                 </Content>
             </Layout>) : (<></>)
             }
--- frontend/src/utils/requests.js
+++ frontend/src/utils/requests.js
@@ -10,36 +10,25 @@ export const getProfile = async() =>{
                 },
             Bearer: localStorage.getItem('userToken')
         });
-        // if (response.ok){
-        // const data = await response.json();
-        // return data;
-        // } else {
-        //     return response;
-        //     // const responseContent = (
-        //     //     <>
-        //     //         <h>Please Login</h>
-        //     //         <br></br>
-        //     //         <h>Redirecting...</h>
-        //     //     </>
-        //     // );
-        //     // message.error(responseContent, 2)
-        //     // .then(() => {
-        //     //     window.location.href = "/login";
-        //     // });
-        // }
         return response;
     } catch (error){
         return error;
-        // const responseContent = (
-        //     <>
-        //         <h>Please Login</h>
-        //         <br></br>
-        //         <h>Redirecting...</h>
-        //     </>
-        // );
-        // message.error(responseContent, 2)
-        // .then(() => {
-        //     window.location.href = "/login";
-        // });
+    }
+}
+
+export const updateProfile = async(data) =>{
+    try{
+        const response = await fetch  (`${backend_url}/users/update_profile`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                'Authorization': 'Bearer ' + localStorage.getItem('userToken')
+                },
+            Bearer: localStorage.getItem('userToken'),
+            body: JSON.stringify(data)
+        });
+        return response;
+    } catch (error){
+        return error;
     }
 }
