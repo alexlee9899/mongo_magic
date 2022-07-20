1cc5f21f4a7082bfefddfdfc1e79aba5a83eac6f
Yu Liang <liangyusam@gmail.com>
2022-07-20 22:16:17 +1000

    question list

--- backend/controller/questions.py
+++ backend/controller/questions.py
@@ -1,10 +1,15 @@
 from flask import request, Blueprint
 from flask_jwt_extended import jwt_required
 
-from services.service_questions import question_add
+from services.service_questions import question_add, question_list
 
 questions_blueprint = Blueprint('question', __name__)
 
 @questions_blueprint.route('/question/addin', methods=['POST'])
 def add():
     return question_add(request.json)
+
+@questions_blueprint.route('/question/list', methods=['GET'])
+@jwt_required()
+def get_list():
+    return question_list(request)
