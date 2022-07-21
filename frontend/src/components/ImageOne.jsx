af6eb597044f1fcd6208e2dbe343510cb94686c9
Yu Liang <liangyusam@gmail.com>
2022-07-21 19:10:39 +1000

    image format

--- frontend/src/components/ImageOne.jsx
+++ frontend/src/components/ImageOne.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Parallax } from 'react-parallax';
 
 const ImageOne = () => {
-  return <Parallax className='image' blur={0} bgImage={require('./image/banner1.jpeg')} strength={800} bgImageStyle={{minHeight:"100vh"}}>
+  return <Parallax className='image' blur={0} bgImage={require('./image/banner1.jpg')} strength={800} bgImageStyle={{minHeight:"100vh"}}>
      <div className='content'>
             <span className="img-txt">Taking the leading position in sustainability</span>
         </div>
