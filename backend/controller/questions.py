d054014929c8ec94e338b015915051216d9a8193
Yu Liang <liangyusam@gmail.com>
2022-07-28 21:00:58 +1000

    answer api

--- backend/controller/questions.py
+++ backend/controller/questions.py
@@ -1,7 +1,7 @@
 from flask import request, Blueprint
 from flask_jwt_extended import jwt_required
 
-from services.service_questions import question_add, question_list
+from services.service_questions import question_add, question_list, question_answer
 
 questions_blueprint = Blueprint('question', __name__)
 
@@ -13,3 +13,8 @@ def add():
 @jwt_required()
 def get_list():
     return question_list(request)
+
+@questions_blueprint.route('/question/answer', methods=['POST'])
+@jwt_required()
+def get_answer():
+    return question_answer(request.json)
