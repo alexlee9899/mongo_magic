de472a7dc150eb6f18ebeb0d94e131601d441036
jinl96 <jinyangliu1996@gmail.com>
2022-08-05 06:06:16 +0800

    copied home page elements into result page

--- frontend/src/pages/AssessmentResultPage/AssessmentResultPage.jsx
+++ frontend/src/pages/AssessmentResultPage/AssessmentResultPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 import { Layout } from 'antd'
 import styled from 'styled-components';
 import { Button } from 'antd';
@@ -9,6 +9,7 @@ import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
+import logo from '../../assets/LogoBlue.png';
 
 const TopNavContainer = styled.div`
 display: flex;
@@ -28,7 +29,7 @@ const ResultCardContainer = styled.div`
     height: 800px;
     width: 1000px;
     background: hsla(0,0%,100%,.95);
-    margin-top: 100px;
+    margin-top: 30px;
     margin-bottom: 200px;
     border-radius: 10px;
     border: solid #89c5d1;
@@ -40,7 +41,6 @@ const ContentContainerRight = styled.div`
     margin: auto;
     height: 100%;
     margin-right: 10px;
-    margin-top: 30px;
 `
 
 const ContentContainerLeft = styled.div`
@@ -49,9 +49,9 @@ const ContentContainerLeft = styled.div`
     width: 400px;
     display: flex;
     flex-direction: column;
-    margin-left: 50px;
     margin-top: 100px;
     margin-right: 50px;
+    text-align: center;
 `
 const ParamContext = styled.span`
     color: #4D7393;
@@ -63,6 +63,45 @@ const TextContext = styled.p`
     margin-left: 5px;
 `
 
+const Navbar = styled.div`
+    font-weight: 600;
+    font-size: 30px;
+    line-height: 40px;
+    letter-spacing: 0.02em;
+    color: #ffffff;
+    display: flex;
+    flex-direction: row;
+    max-width: 1200px;
+    margin: auto;
+    justify-content: space-between;
+    padding-top: 1.2rem;
+`
+
+const Logoimg = styled.img`
+    width: 56px;
+    height:56px;
+    `
+
+
+const Atag = styled.a`
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 22px;
+    /* identical to box height */
+    text-align: center;
+    color: #ffffff;
+    margin: 1rem;
+`
+
+const Span = styled.span`
+  right: 10%;
+  top: 1rem;
+  margin-left: 1rem;
+  display: flex;
+  flex-direction: row;
+`
+
+
 
 const AssessmentResultPage = () => {
     const { id } = useParams();
@@ -71,6 +110,7 @@ const AssessmentResultPage = () => {
     const [loading, setLoading] = useState(true);
     const [preparing, setPreparing] = useState(true);
     const [data, setData] = useState({});
+    const navigate = useNavigate();
 
     const themeColor_light = '#89c5d1';
 
@@ -118,6 +158,42 @@ const AssessmentResultPage = () => {
             , Math.random() * (600 - 1) + 1));
     }
 
+
+    const turnToLogin = () => {
+        navigate('/login');
+    }
+
+    const turnToRegister = () => {
+        navigate('/signup');
+    }
+
+    const handleClick = () => {
+        if (localStorage.getItem('userToken')) {
+            navigate('/assessment');
+        }
+        else {
+            turnToLogin();
+        }
+    }
+
+    const turnToDashboard = () => {
+        navigate('/users/dashboard');
+    }
+
+    const logout = () => {
+        localStorage.removeItem('userToken');
+        navigate('/');
+    }
+
+    const turnToAbout = () => {
+        navigate('/about')
+    }
+
+    const turnToHelp = () => {
+        navigate('/help')
+    }
+
+
     const bulletPoints = () => {
         const suggestion = data['suggestion'];
         // return(
@@ -153,12 +229,12 @@ const AssessmentResultPage = () => {
         const element = document.getElementById('contentCard');
         const w = element.offsetWidth;
         const h = element.offsetHeight;
-        const offsetTop = element.offsetTop;   
+        const offsetTop = element.offsetTop;
         const offsetLeft = element.offsetLeft;
         const canvas = document.createElement("canvas");
         let abs = 0;
         const win_i = document.body.clientWidth;
-        const win_o = window.innerWidth; 
+        const win_o = window.innerWidth;
         if (win_o > win_i) {
             abs = (win_o - win_i) / 2;
         }
@@ -170,7 +246,7 @@ const AssessmentResultPage = () => {
         context.translate(-offsetLeft - abs, -offsetTop);
         html2canvas(element, {
             allowTaint: true,
-            scale: 2 
+            scale: 2
         }).then(canvas => {
             const contentWidth = canvas.width;
             const contentHeight = canvas.height;
@@ -196,7 +272,6 @@ const AssessmentResultPage = () => {
         })
 
     }
-
 
 
 
@@ -227,27 +302,55 @@ const AssessmentResultPage = () => {
     return (
         <>
             <Parallax className='image' blur={0} bgImage={require('../../assets/banner1.jpg')} strength={800} bgImageStyle={{ minHeight: "100vh" }} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                <ResultCardContainer style={{ justifyContent: loading ? 'center' : '' }}>
-                    <div id="contentCard" style={{ justifyContent: 'center', display:'flex' }}>
-                    {(loading) ? (<div style={{ width: '150px', height: '150px', alignSelf: 'center' }}><CircularProgressbar text={value < 100 ? `${value}%` : preparing ? `100%` : `Ready`} value={value} /></div>) :
-                        (<ContentContainer>
-                            <ContentContainerLeft>
-                                <div style={{ height: '400px', width: '100%', backgroundColor: `${themeColor_light}` }}>Picture</div>
-                                <TextContext style={{ display: 'block', whiteSpace: 'normal', overflowWrap: 'break-word', color: '#89c5d1', fontWeight: '600' }}>
-                                    Your organisation's annual carbon footprint is <ParamContext style={{ color: '#4D7393' }}>{data.co2}</ParamContext> tonnes of Carbon Dioxide equivalent (tCO<sub>2</sub>e).
-                                    To compensate for your emissions, around <ParamContext>{data.natural_habitat}</ParamContext>m<sup>2</sup> of natural habitat must be restored.
-                                </TextContext>
-                            </ContentContainerLeft>
-                            <ContentContainerRight>
-                                <div style={{height:'100px', width:'100%', display:'flex', justifyContent:'end'}}>
-                                    <Button style={{ borderRadius:'5px', width:'150px', alignSelf:'end', marginRight:'20px', marginBottom:'50px' ,color: '#4D7393'}} onClick={downloadPdf} data-html2canvas-ignore="true">Save as PDF</Button>
-                                </div>
-                                <h3 style={{ display: 'block', whiteSpace: 'normal', overflowWrap: 'break-word', color: '#4D7393', fontWeight: '600', marginLeft: '20px', fontSize: '32px' }} >Result</h3>
-                                <h1 style={{ marginLeft: '20px', color: '#4D7393' }}>Your Organisation's Scored <ParamContext style={{ fontSize: '30px', color: '#89c5d1' }}>99</ParamContext> in our assessment</h1>
-                                <div>{bulletPoints()}</div>
-                            </ContentContainerRight>
-                        </ContentContainer>)}
+                <Navbar>
+                    <div className='logo-title'>
+                        <Logoimg src={logo} alt="logo" />
+                        <div className='title'>G'Tracker </div>
                     </div>
+                    <Span>
+                        <Atag>Home</Atag>
+                        <Atag>Ranking</Atag>
+                        <Atag onClick={turnToHelp}>Help</Atag>
+                        <Atag onClick={turnToAbout}>About</Atag>
+                        {
+                            localStorage.getItem('userToken') ?
+                                <div style={{ margin: '1rem', lineHeight: '0' }}>
+                                    <Atag onClick={turnToDashboard}>Dashboard</Atag>
+                                    <Atag onClick={logout}>Logout</Atag>
+                                </div>
+                                :
+                                <div style={{ margin: '1rem', lineHeight: '0' }}>
+                                    <Atag onClick={turnToLogin}>Login</Atag>
+                                    <Atag onClick={turnToRegister}>Sign up</Atag>
+                                </div>
+                        }
+                    </Span>
+                </Navbar>
+                <ResultCardContainer style={{ justifyContent: loading ? 'center' : '' }}>
+                    <div id="contentCard" style={{ justifyContent: 'center', display: 'flex' }}>
+                        {(loading) ? (<div style={{ width: '150px', height: '150px', alignSelf: 'center' }}><CircularProgressbar text={value < 100 ? `${value}%` : preparing ? `100%` : `Ready`} value={value} /></div>) :
+                            (<ContentContainer>
+                                <ContentContainerLeft>
+                                    <div style={{ marginLeft: '25px', height: '400px', width: '100%', backgroundColor: `${themeColor_light}` }}>Picture</div>
+                                    <TextContext style={{ marginLeft: '25px', display: 'block', whiteSpace: 'normal', overflowWrap: 'break-word', color: '#89c5d1', fontWeight: '600' }}>
+                                        Your organisation's annual carbon footprint is <ParamContext style={{ color: '#4D7393' }}>{data.co2}</ParamContext> tonnes of Carbon Dioxide equivalent (tCO<sub>2</sub>e).
+                                        To compensate for your emissions, around <ParamContext>{data.natural_habitat}</ParamContext>m<sup>2</sup> of natural habitat must be restored.
+                                    </TextContext>
+                                </ContentContainerLeft>
+                                <ContentContainerRight>
+                                    <div style={{ height: '100px', width: '100%', display: 'flex', justifyContent: 'end' }}>
+                                        <Button style={{ borderRadius: '5px', width: '150px', alignSelf: 'end', marginRight: '10px', marginBottom: '50px', color: '#4D7393', borderColor: '#89c5d1' }} onClick={downloadPdf} data-html2canvas-ignore="true">Save as PDF</Button>
+                                    </div>
+                                    <h3 style={{ display: 'block', whiteSpace: 'normal', overflowWrap: 'break-word', color: '#4D7393', fontWeight: '600', marginLeft: '20px', fontSize: '32px' }} >Result</h3>
+                                    <h1 style={{ marginLeft: '20px', color: '#4D7393' }}>Your Organisation's Scored <ParamContext style={{ fontSize: '30px', color: '#89c5d1' }}>99</ParamContext> in our assessment</h1>
+                                    <div>{bulletPoints()}</div>
+                                    <div style={{ display: 'flex', height: '100%', width: 'auto', justifyContent: 'flex-end', alignItems: 'flex-end' }}>
+                                        <div style={{ position: 'relative', left: '-80px', alignSelf: 'flex-end', whiteSpace: 'nowrap', color: '#4D7393', fontWeight: '600', marginRight: '5px' }}>By G'Tracker</div>
+                                    </div>
+                                </ContentContainerRight>
+                            </ContentContainer>)}
+                    </div>
+                    {/* { !!!loading && <div style={{ alignSelf:'flex-end', whiteSpace:'nowrap', color:'#4D7393',fontWeight:'600', marginRight:'5px' }}>By G'Tracker</div>} */}
                 </ResultCardContainer>
             </Parallax>
         </>
