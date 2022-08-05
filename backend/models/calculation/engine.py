12791e5ed64f6e5d5858c1e3eb1af845a5c1b69b
Yu Liang <liangyusam@gmail.com>
2022-08-05 15:55:07 +1000

    calculation engine

--- backend/models/calculation/engine.py
+++ backend/models/calculation/engine.py
@@ -5,10 +5,11 @@ def engine(office, data):
   if office:
     co2e = 0
     habitat = 0
+    employee = 0
+    total_floor = 0
+    total_electricity = 0
     for item in office:
       electricity = 0
-      employee = 0
-      floor = 0
       green_energy_percent = 0
       total = 0
       postcode = 0
@@ -24,9 +25,13 @@ def engine(office, data):
         elif key == "office_postcode":
           postcode = item[key]
         else:
-          total = eval(f'{key}("{item[key]}")')
-      office_result[employee] = 
+          total += eval(f'{key}("{item[key]}")')
+      employee += employee
+      total_floor += floor
       co2e += calculate_co2(electricity, green_energy_percent, postcode)
       habitat += calculate_habitat(co2e)
+      total_electricity += electricity
+    avg_energy = total_electricity / total_floor /employee
+    
       
   return None
